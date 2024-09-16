e5e983c1f97e34b9a0fff7a28c189b9d1445764a
VictorGM01 <victormarques8801@gmail.com>
2024-09-16 13:45:08 -0300

    feat: adiciona IP no corpo da requisição

--- src/backend/src/core/controllers/logController.js
+++ src/backend/src/core/controllers/logController.js
@@ -4,28 +4,37 @@ require('dotenv').config();
 const LOG_SERVICE_URL = process.env.LOG_SERVICE_URL;
 
 async function getAllController(req, res) {
-    try {
-        const response = await axios.get(`${LOG_SERVICE_URL}/webhook/logs/external`);
-        res.json(response.data);
-    } catch (err) {
-        res.status(err.response ? err.response.status : 500).json({
-            error: err.response ? err.response.data.message : 'Internal server error',
-        });
-    }
-    }
+  try {
+    const response = await axios.get(
+      `${LOG_SERVICE_URL}/webhook/logs/external`
+    );
+    res.json(response.data);
+  } catch (err) {
+    res.status(err.response ? err.response.status : 500).json({
+      error: err.response ? err.response.data.message : 'Internal server error',
+    });
+  }
+}
 
 async function createController(req, res) {
-    try {
-        const response = await axios.post(`${LOG_SERVICE_URL}/webhook/logs/external`, req.body);
-        res.json(response.data);
-    } catch (err) {
-        res.status(err.response ? err.response.status : 500).json({
-            error: err.response ? err.response.data.message : 'Internal server error',
-        });
-    }
+  try {
+    const data = {
+      ip: req.headers['x-forwarded-for'] || req.socket.remoteAddress,
+      ...req.body,
+    };
+    const response = await axios.post(
+      `${LOG_SERVICE_URL}/webhook/logs/external`,
+      data
+    );
+    res.json(response.data);
+  } catch (err) {
+    res.status(err.response ? err.response.status : 500).json({
+      error: err.response ? err.response.data.message : 'Internal server error',
+    });
+  }
 }
 
 module.exports = {
-    getAllController,
-    createController,
+  getAllController,
+  createController,
 };
